053cd900adc9ff4a914debcbaafa1df076255133
Abdulwali <alhammadiabdulwali@gmail.com>
2023-09-29 23:17:31 +0300

    using objects instead of classes

--- js/index.js
+++ js/index.js
@@ -1,38 +1,37 @@
-class Product {
-  constructor(name, details, price, photo) {
-    this.name = name;
-    this.details = details;
-    this.price = price;
-    this.photo = photo;
-  }
-}
-
-function addNewProduct(Product) {
-  productList.push(Product);
-}
-
-function getTotalPrice() {
-  totalPrice = 0;
-  productList.forEach((product) => {
-    totalPrice += product.price;
-  });
-  return totalPrice;
-}
-
-function findProduct(productName) {
-  const regex = new RegExp(productName, "i");
-  return productList.filter((product) => {
-    return product.name.search(regex) >= 0;
-  });
-}
-
+// variables declaration
 const productList = [];
 let totalPrice = 0;
 
-addNewProduct(new Product("P1", "P1_details", 100, null));
-addNewProduct(new Product("P2", "P2_details", 200, null));
-addNewProduct(new Product("P3", "P3_details", 300, null));
+
+// functions declaration
+function addNewProduct(productName, productDetails, productPrice = 0, productPhoto) {
+    let Product = {
+        name: productName,
+        details: productDetails,
+        price: productPrice,
+        photo: productPhoto
+    }
+    productList.push(Product);
+}
+
+function getTotalPrice() {
+    totalPrice = 0;
+    productList.forEach((product) => {
+        totalPrice += product.price;
+    });
+    return totalPrice;
+}
+
+function findProduct(productName) {
+    const regex = new RegExp(productName, "i");
+    return productList.filter(product => product.name.search(regex) >= 0);
+}
+
+// program execution
+addNewProduct("P1", "P1_details", 100, null);
+addNewProduct("P2", "P2_details", 200, null);
+addNewProduct("P3", "P3_details", 300, null);
 
 console.log(productList);
 console.log(getTotalPrice());
-console.log(findProduct("P2"));
+console.log(findProduct("P"));
